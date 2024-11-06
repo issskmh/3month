f04b414177594f3fbca966b3b3292e14fbffcd56
issskmh <meshanlomislam@gmail.com>
2024-11-06 15:45:40 +0600

    medified hw

--- main.py
+++ main.py
@@ -8,17 +8,10 @@ import logging
 from handlers.start import start_router
 from handlers.myinfo import myinfo_router
 from handlers.random import random_router
-
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+from handlers.dotenv import dp, bot
 
 
-async def picture():
 
-    await dp.start_polling(bot)
 
 async def main():
     dp.include_router(start_router)
